9609b6974f89b6ade637b6655ac8afb945f29d77
Edgar <elindo586@gmail.com>
2022-10-04 17:04:12 -0400

    tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/general.css";
-import Head from "next/head";
+
 import MainNav from "../components/nav-main";
 import Footer from "../components/footer";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <div>
-        <Head></Head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=UA-36867189-13"
+        />
+
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-36867189-13');
+            `,
+          }}
+        />
       </div>
       <nav>
         <MainNav />
