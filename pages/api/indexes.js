eaa1eea12a55b9c0865f5d612845226679856e14
Edgar <elindo586@gmail.com>
2023-10-03 13:49:50 -0400

    text loader not working

---
+++ pages/api/indexes.js
@@ -0,0 +1,30 @@
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { CharacterTextSplitter } from "langchain/dist/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { FaissStore } from "langchain/vectorstores/faiss";
+
+let loader;
+let docs;
+let splitter;
+let documents;
+let embeddings;
+let vectorStore;
+
+const indexes = async (req, res) => {
+  loader = new TextLoader("../../utils/restaurant.txt");
+  docs = await loader.load();
+  splitter = new CharacterTextSplitter({
+    chunkSize: 200,
+    chunkOverlap: 50,
+  });
+  documents = await splitter.splitDocuments(docs);
+  console.log(documents);
+
+  embeddings = new OpenAIEmbeddings();
+  vectorStore = await FaissStore.fromDocuments(documents, embeddings);
+  await vectorStore.save("./");
+
+  res.status(200).json({ ok: ok });
+};
+
+export default indexes;
