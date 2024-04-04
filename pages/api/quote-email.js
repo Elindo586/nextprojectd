2e2d38df5b92fc1ad601582ecb5468d2c980bb1b
Edgar <elindo586@gmail.com>
2024-04-04 09:57:16 -0400

    quote email

---
+++ pages/api/quote-email.js
@@ -0,0 +1,60 @@
+export default async function stuff(req, res) {
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({
+    service: "Outlook365",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EPASSWORD,
+    },
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
+  const name = req.body.contact;
+  const email = req.body.email;
+  const title = req.body.id;
+  const quote = req.body.quote;
+
+  const textArray = quote;
+  const contents = textArray
+    .map(
+      ({ QuoteN, Part, PartDesc, Qty, Price, ExtPrice, LeadTime }) =>
+        `Quote Number: ${QuoteN} \nQyt ${Qty}, Part: ${Part}, Descrition: ${PartDesc}, Price: ${Price}, Ext Price: ${ExtPrice}, Lead Time: ${LeadTime}\n \n`
+    )
+    .join("\n");
+
+  const text = `hola ${name},\n\nTodo bien con su cotizacion? Necesita algo mas? \n \n${contents} \n \nSaludos, \n \nEdgar \n`;
+
+  console.log(text);
+
+  const mailData = {
+    from: "info@tu.biz",
+    to: email,
+    subject: `cotizaciones para ${name} | ${title}`,
+    text: `${text}`,
+    html: `<div><pre>${text}</pre></div>`,
+  };
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log("email sent");
+        resolve(info);
+      }
+    });
+  });
+}
---
+++ pages/file.js
@@ -0,0 +1,84 @@
+import React from "react";
+import quotes from "../thejsons/test-quote.json";
+
+export default function ContactForm() {
+  const handleSubmit = async () => {
+    const d = new Date();
+    const month = d.getMonth();
+    const days = d.getDate();
+    const year = d.getFullYear();
+    const hour = d.getHours();
+    const minutes = d.getMinutes();
+    const seconds = d.getSeconds();
+
+    var timesRun = 0;
+    var arrayIndex = 0;
+
+    console.log(
+      `We are moving on ${month}/${days}/${year} at ${hour}:${minutes}:${seconds}s`
+    );
+
+    var myIntervals = setInterval(async function () {
+      const currentQuote = quotes[arrayIndex++];
+      const data = {
+        contact: currentQuote.Contact,
+        email: currentQuote.Email,
+        quote: currentQuote.quote,
+        id: currentQuote.id,
+      };
+
+      console.log(data);
+
+      if (arrayIndex === quotes.length) {
+        clearInterval(myIntervals);
+        arrayIndex = 0;
+        console.log("We are done!");
+      }
+      const d = new Date();
+      const month = d.getMonth();
+      const days = d.getDate();
+      const year = d.getFullYear();
+      const hour = d.getHours();
+      const minutes = d.getMinutes();
+      const seconds = d.getSeconds();
+      timesRun += 1;
+      console.log(
+        `we ran ${timesRun} times at ${month}/${days}/${year} at ${hour}:${minutes}:${seconds}s`
+      );
+
+      console.log(arrayIndex, quotes.length);
+
+      await fetch("/api/quote-email", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          console.log("Response received");
+
+          if (res.status === 200) {
+            console.log("Response succeeded!");
+          }
+        })
+        .catch((error) => {
+          console.log("Error", error);
+        });
+    }, 100 * 1000);
+  };
+
+  return (
+    <div>
+      <div className="col-md-12 main-contact-form-div">
+        <h4 className="get-in-touch">Get in touch.</h4>
+        <br />
+        <br />
+        <button className="btn btn-primary form-button" onClick={handleSubmit}>
+          click me
+        </button>
+      </div>
+    </div>
+  );
+}
