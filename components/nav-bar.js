777d38e813fb4868d055f21b8db32ea1c98bbb79
Edgar <elindo586@gmail.com>
2024-07-19 12:19:51 -0400

    navbar

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -1,48 +1,110 @@
+import { React, useState, useEffect } from "react";
 import Link from "next/link";
 
-const NavBar = () => {
+function MainNav() {
+  const [isPath, setIsPath] = useState();
+  useEffect(() => {
+    title();
+  }, [isPath]);
+
+  const show = () => {
+    let drop = document.getElementById("navbarTogglerDemo02");
+
+    if (drop.classList.value === "navbar-collapse collapse") {
+      drop.classList.value = "navbar-collapse collapse show";
+    } else {
+      drop.classList.value = "navbar-collapse collapse";
+    }
+  };
+
+  const title = () => {
+    let findTitle = window.location.pathname;
+    setIsPath(findTitle);
+    // document.title = `TU  | ${findTitle}`;
+    switch (findTitle) {
+      case "/":
+        document.title = "Edgar Lindo | Home";
+        break;
+      case "/about":
+        document.title = "Edgar Lindo | About";
+        break;
+      case "/coding":
+        document.title = "Edgar Lindo | Coding";
+        break;
+      case "/work":
+        document.title = "Edgar Lindo | Work";
+        break;
+      case "/contact":
+        document.title = "Edgar Lindo | Contact";
+        break;
+
+      default:
+        console.log("This is not a part of this website.");
+    }
+  };
+
   return (
-    <div className="main-navbar bg-dark">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-main">
-        <a className=" text-white navbar-brand" href="#">
-          Edgar Lindo
-        </a>
+    <div className="bg-dark ">
+      <nav
+        className="navbar navbar-expand-lg bg-dark navbar-dark main-navbar "
+        onClick={title}
+      >
+        <Link legacyBehavior href="/">
+          <a onClick={title} className="navbar-brand ">
+            &nbsp; &nbsp; <strong>Edgar Lindo</strong>
+          </a>
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          onClick={show}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+
+        <div className="navbar-collapse collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <Link className="nav-link" href="/">
-                <span className=" text-secondary sr-only">Home</span>
+              <Link legacyBehavior href="/">
+                <a className="nav-link" onClick={title}>
+                  Home
+                </a>
               </Link>
             </li>
-            <li className=" nav-item">
-              <Link className="text-secondary  nav-link" href="/about">
-                About
-              </Link>
-            </li>
-            <li className="text-secondary nav-item">
-              <Link className="text-secondary nav-link" href="/coding">
-                Coding
-              </Link>
-            </li>
+
             <li className="nav-item">
-              <Link className="text-secondary nav-link" href="/work">
-                Work
+              <Link legacyBehavior href="/about">
+                <a className="nav-link" onClick={title}>
+                  About
+                </a>
               </Link>
             </li>
+
             <li className="nav-item">
-              <Link className=" text-secondary nav-link " href="/contact">
-                Contact
+              <Link legacyBehavior href="/coding">
+                <a className="nav-link" onClick={title}>
+                  Coding
+                </a>
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link legacyBehavior href="/work">
+                <a className="nav-link">Work</a>
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link legacyBehavior href="/contact">
+                <a className="nav-link" onClick={title}>
+                  Contact
+                </a>
               </Link>
             </li>
           </ul>
@@ -50,5 +112,6 @@ const NavBar = () => {
       </nav>
     </div>
   );
-};
-export default NavBar;
+}
+
+export default MainNav;
---
+++ pages/test.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Text from "../components/text";
+import db from "../utils/text.json";
+
+// const hiddenStyles = {
+//   position: "absolute",
+//   overflow: "hidden",
+//   clip: "rect(0 0 0 0)",
+//   height: 1,
+//   width: 1,
+//   margin: -1,
+//   padding: 0,
+//   border: 0,
+// };
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.count("submit");
+        }}
+      >
+        <input type="text" />
+        <button type="submit">btn</button>
+      </form>
+
+      <div>
+        <div> Le comento de productos en STOCK:</div>
+        <div>
+          {" "}
+          <u> Bombas de pistones y repuestos:</u>
+        </div>
+      </div>
+    </div>
+  );
+}
